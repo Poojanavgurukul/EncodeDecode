15e3317593449b31832f0bd29fb6f3997f862d6e
Poojanavgurukul <Pooja17@navgurukul.org>
2019-08-06 15:15:39 +0530

    Add.Test cases for encode and decode

--- src/test/java/encodeDecodeTest/EncodeDecodeTest.java
+++ src/test/java/encodeDecodeTest/EncodeDecodeTest.java
@@ -1,6 +1,29 @@
 package encodeDecodeTest;
 
+import encodeDecode.Decode;
+import encodeDecode.Encode;
+import encodeDecode.EncodeDecodeTable;
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
+import static junit.framework.TestCase.assertEquals;
+
 public class EncodeDecodeTest {
+    @Test public void stringShouldEncodeInBinary(){
+        List<String> expexted= Arrays.asList("01100110", "01101111", "01101111");
+        String userInput = "foo";
+        Encode encode=new Encode();
+        assertEquals(expexted,encode.encoder(userInput, EncodeDecodeTable.generateBinaryTable()));
+    }
+    @Test public void encodedMessageShouldDecode(){
+        String expected="foo";
+        String userInput = "foo";
+        Encode encode=new Encode();
+        Decode decode=new Decode();
+        List<String> encodedData = encode.encoder(userInput, EncodeDecodeTable.generateBinaryTable());
+        String decodeData = decode.decoder(encodedData, EncodeDecodeTable.generateBinaryTable());
+        assertEquals(expected,decodeData);
+    }
 }
