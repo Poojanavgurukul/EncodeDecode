59153650743adedf9c2d36ed42a145a562c8c9f3
Poojanavgurukul <Pooja17@navgurukul.org>
2019-08-09 22:01:23 +0530

    Add.test case for getting byte that character can represent

--- src/test/java/encodeDecodeTest/BitsBytesTest.java
+++ src/test/java/encodeDecodeTest/BitsBytesTest.java
@@ -1,6 +1,7 @@
 package encodeDecodeTest;
 
 import encodeDecode.Bits;
+import encodeDecode.Bytes;
 import encodeDecode.UniqueCharacter;
 import org.junit.Test;
 
@@ -12,5 +13,11 @@ public class BitsBytesTest {
         UniqueCharacter uniqueCharacter=new UniqueCharacter();
         int expected = 3;
         assertEquals(expected,bits.getBits(uniqueCharacter.getUniqueCharacter("ankita").size()));
+    }
+    @Test public void itShouldGiveBytesInWhichCharacterCanRepresent(){
+        Bytes bytes=new Bytes();
+        UniqueCharacter uniqueCharacter=new UniqueCharacter();
+        int expected=8;
+        assertEquals(expected,bytes.getBits(uniqueCharacter.getUniqueCharacter("ankita").size()));
     }
 }
